0e5119093e062e033c9e316691f953b948da23ae
Ubermansch-learner <Sumanswain983@gmail.com>
2024-10-15 13:06:31 +0000

    conversions and operations-prefix,postfix increment and decrement

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,61 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)//for converting it to boolean
 
 let someNumber= 33
 let stringnumber= String(someNumber)
-console.log(typeof stringnumber);
-console.log(stringnumber);
+// console.log(typeof stringnumber);
+// console.log(stringnumber);
+
+
+//******************************** operations *********************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**3);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2= " World"
+
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4) * 5 % 3);
+
+
+
+// console.log( true);
+// console.log( +true);
+// console.log( +"");
+
+//+ initially act as conversion and converts true to 1,"" to 0
+
+let num1, num2, num3
+num1= num2 =num3 = 2 + 2
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+console.log(x);
+console.log(y);
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 4n
+console.log(x2);
+console.log(y2);
+
+
+
+
+
+
