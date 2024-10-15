1ffba8e44a377c4424f4b3a5db2ec2e68a550cee
Ubermansch-learner <Sumanswain983@gmail.com>
2024-10-15 12:33:20 +0000

    Basic conversion of difft datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score ="33"
+let score2="33abc"
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+
+let valueInNumber2 = Number(score2)
+
+//console.log(typeof(valueInNumber2));
+//console.log(valueInNumber2);
+//NaN=not a number
+
+//"33"=>33
+//"33abc"=>NaN
+// true=>1; false=>0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)//for converting it to boolean
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+
+//1=>true
+//""=>false
+//"suman"=>true
+
+
+let someNumber= 33
+let stringnumber= String(someNumber)
+console.log(typeof stringnumber);
+console.log(stringnumber);
