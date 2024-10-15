9087c6ef7274ce4288580bf7476a50d641463916
Ubermansch-learner <Sumanswain983@gmail.com>
2024-10-15 12:12:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  //we are using nodejs, not browser therefore will give error
+
+let name="suman"
+let age= 30
+let isLoggedIn=false
+let state=null
+let table;
+
+//primitive data types
+
+//number => 2 t0 power of 53
+//bigInt
+
+//string=> prefer using in " "
+// boolean=> T /F
+//null =>standalone value
+//undefined=>
+    //symbol=> used for uniqueness
+
+
+
+
+//object
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
