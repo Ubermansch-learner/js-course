d5a794318706ab10aa319320cd408ae93483f4cd
Ubermansch-learner <Sumanswain983@gmail.com>
2024-10-16 06:56:16 +0000

    math and nums in js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,46 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// //explicitly definining it as number data type
+
+// console.log(balance.toString());
+// // after converting it to string, u can use methods available for string like-see below
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//precision value
+// //usually for e commerce app
+
+// const otherNumber= 23.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const otherNumber2= 123.8966
+// console.log(otherNumber2.toPrecision(3));
+
+// const otherNumber3= 1123.8966
+// console.log(otherNumber3.toPrecision(4));
+
+// const hundreds=1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+
+console.log(Math.random());
+// will always lie b/w 0 and 1
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max=20;
+console.log(Math.ceil(Math.random() * (max-min +1) + min ))
+
+
+
