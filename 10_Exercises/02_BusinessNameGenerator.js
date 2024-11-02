de907ca3262989bfa8d59ecf580dec8b0ec8de52
Ubermansch-learner <Sumanswain983@gmail.com>
2024-11-02 17:45:06 +0000

    Random Name Generaor

--- 10_Exercises/02_BusinessNameGenerator.js
+++ 10_Exercises/02_BusinessNameGenerator.js
@@ -15,3 +15,36 @@ hub
 
 
 */
+
+let random1= Math.random();
+let word1;
+if(random1<1/3){
+    word1="crazy";
+}else if(random1<2/3){
+    word1="amazing";
+}else{
+    word1="fire";
+}
+
+let random2=Math.random();
+let shopname;
+if(random2<1/3){
+    shopname="engine";
+}else if(random2<2/3){
+    shopname="food";
+}else{
+    shopname="garments";
+}
+
+let random3=Math.random();
+let anotherWord;
+if(random3<1/3){
+    anotherWord="bros";
+}else if(random3<2/3){
+    anotherWord="limited";
+}else{
+    anotherWord="hub";
+}
+
+const BusinessNamegenerator = `${word1} ${shopname} ${anotherWord}`;
+console.log(BusinessNamegenerator);
