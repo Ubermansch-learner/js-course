2c85e5f7d59f701b141b06fcd59e36594e3ba253
Ubermansch-learner <Sumanswain983@gmail.com>
2024-11-06 17:42:11 +0000

    various ways

--- 10_Exercises/04_Factorial.js
+++ 10_Exercises/04_Factorial.js
@@ -1,21 +1,30 @@
 //using reduce and for loops
 
 //5!
-let element=1;
-for (let i = 5; i < 0; i--) {
-     element *= i;   
+// let element=1;
+// for (let i = 5; i < 0; i--) {
+//      element *= i;   
+// }
+// console.log(element);
+
+
+// let arr=[5,4,3,2,1];
+// const fact =(a,b)=>{
+
+//     return a*b;
+
+// }
+// console.log(arr.reduce(fact));
+
+
+
+let a =6
+function factorial(number){
+    let arr=Array.from(Array(number+1).keys())//returns an array from 1 till 6 as we have also used slice later
+    console.log(arr.slice(1,))
+    let c=arr.slice(1,).reduce((a,b)=>{
+        return a*b
+    })
+    console.log(c)
 }
-console.log(element);
-
-
-let arr=[5,4,3,2,1];
-const fact =(a,b)=>{
-
-    return a*b;
-
-}
-console.log(arr.reduce(fact));
-
-
-
-
+factorial(a);
